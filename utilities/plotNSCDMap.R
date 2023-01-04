493e0ba180e9301f1dcc21a3ba5d81ed1ffd1fe0
Teixeira <Estevam.Teixeira@iwk.nshealth.ca>
2023-01-04 19:59:34 -0400

    conditional heart defects + tm_text

--- utilities/plotNSCDMap.R
+++ utilities/plotNSCDMap.R
@@ -9,6 +9,15 @@ plot_map <- function(data){
                   #bins = intv)
   )
   
+  txt <- dta %>% 
+    dplyr::group_by(time_lab) %>% 
+    dplyr::filter(rate > as.numeric(quantile(rate, probs  = 0.75, na.rm = TRUE))) %>% 
+    dplyr::ungroup() |>
+    dplyr::mutate(name = gsub(" ", "\n", name)) |>
+    sf::st_sf() |>
+    sf::st_transform(4326)
+    
+  
  map <- tm_shape(dta) +
     tm_fill("rate",
             style = "pretty",
@@ -24,29 +33,34 @@ plot_map <- function(data){
             # popup.format = dta[["hover.format"]]
     ) +
     tm_borders(lwd = 0.01, col = "white") +
-    tm_layout(title = unique(dta[["time_lab"]]),
-              title.size = 0.9,
-              bg.color = "#DDDDDD",
+    tm_facets(by = "time_lab",
+              nrow = 2, sync = TRUE) +
+    tm_layout(bg.color = "#DDDDDD",
               fontface = "bold",
-              frame = "white",
+              frame = NA,
               panel.label.bg.color = NA,
-              # frame.lwd = NA,
+              panel.label.size = 1.5,
+              panel.label.fontface = "bold",
+              frame.lwd = NA,
               inner.margins = c(0,0,0,0),
               outer.margins = c(0,0,0,0),
-              legend.position = c("right","bottom"),
-              legend.just = c("right","bottom"),
-              legend.title.size = 0.7,
-              legend.text.size = 0.6,
-              legend.outside.size = 0.6
-    ) 
-    # tm_text("name",
-    #         size = 0.65,
-    #         fontface = "bold",
-    #         # col = "black",
-    #         # size = "area",
-    #         legend.size.show = FALSE,
-    #         auto.placement = FALSE
-    #         )
+              legend.position = c("center","center"),
+              legend.just = c("center","center"),
+              # legend.title.size = 0.7,
+              legend.text.size = 0.8
+              # legend.outside.size = 0.8
+    ) +
+   tm_shape(txt) +
+   tm_facets(by = "time_lab",
+             nrow = 2, sync = TRUE) +
+    tm_text("name",
+            size = 1,
+            fontface = "bold",
+            col = NA,
+            # size = "area",
+            legend.size.show = FALSE,
+            auto.placement = FALSE
+            )
   
  return(map)
 }
